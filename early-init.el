6e720ffa40c4f71ffc35dc8600194e1473ab5ec5
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2022-04-01 15:31:35 +0300

    Update gc threshold to make startup faster

--- early-init.el
+++ early-init.el
@@ -1,2 +1,6 @@
 (add-to-list 'default-frame-alist '(undecorated . t))
 (setq straight-check-for-modifications '(check-on-save find-when-checking))
+
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
