eb96f451621c309df9153f5aca695808604ee051
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2022-04-02 05:48:52 +0300

    Update GC settings to restore its value to normal after startup

--- early-init.el
+++ early-init.el
@@ -1,6 +1,13 @@
 (add-to-list 'default-frame-alist '(undecorated . t))
 (setq straight-check-for-modifications '(check-on-save find-when-checking))
 
-;; The default is 800 kilobytes.  Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
+;; Set the threshold to very high to not garbage collect
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Restore `gc-cons-threshold' to a reasonable value after init
+(add-hook 'emacs-startup-hook  ; Hook that runs after init files are loaded
+          (lambda ()
+            ;; Set a reasonable value that won't hang Emacs or won't increase ram usage
+            (setq gc-cons-threshold (* 50 1000 1000)))
+          100)  ; Make sure this function will run at the end of `emacs-startup-hook's.
 
