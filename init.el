ca8dcc30655297fa145de8026ea1f60cf4aa6c5d
Seyyid Ibrahim Gulec <seyyidibrahimgulec@gmail.com>
2022-04-17 01:48:57 +0300

    Add org-roam package

--- init.el
+++ init.el
@@ -368,6 +368,21 @@
   :defer t
   :hook (org-mode . ig/org-mode-visual-fill))
 
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory "~/RoamNotes")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert)
+         :map org-mode-map
+         ("C-M-i" . completion-at-point))
+  :config
+  (org-roam-setup))
+
 (use-package yasnippet
   :hook (prog-mode . yas-minor-mode)
   :config
